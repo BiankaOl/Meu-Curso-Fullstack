c5886626fa9032b290d32ceb951fa4df0b3c4319
BiankaOl <bianksales17@gmail.com>
2024-02-21 11:16:01 -0300

    updat

--- Módulo 02/07 - Interação com API/Aula 05/Exemplo-01/exemplo-01.js
+++ Módulo 02/07 - Interação com API/Aula 05/Exemplo-01/exemplo-01.js
@@ -3,8 +3,21 @@ async function buscar(){
     let produtos = await procura.json()
     
     let listDiv = document.getElementById("lista_card")
-    for(let x in produtos){
-        listDiv.innerHTML = `<h1> ${produtos[x].nome} </h1>`
+    for(let produto of produtos){
+        listDiv.innerHTML+=`
+            <div class = "card">
+                <div class= "grupo_img">
+                    <img src="${produto.img}" width= "auto" heigth= "auto">
+                </div>
+                <div class= "textos">
+                    <h3> ${produto.nome} </h3>
+                    <p> ${produto.descricao} </p>
+                <div>
+                    <span class= "Com"> R$ ${(produto.valorComDesconto).toFixed(2).replace(".", ",")}</span>
+                    <span class= "Sem"> R$ ${(produto.valorSemDesconto).toFixed(2).replace(".", ",")}</span>
+                </div>
+            </div>
+        `
     }
 }
 
